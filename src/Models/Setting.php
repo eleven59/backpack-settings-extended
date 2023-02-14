fd65640c991d80948aaa1646ce25f0814bc285a2
Nik Linders <info@eleven59.nl>
2023-02-14 14:13:36 +0100

    Minor bugfix for long strings

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -20,7 +20,7 @@ class Setting extends \Backpack\Settings\app\Models\Setting
                 'quality' => config('eleven59.backpack-settings-extended.image.quality'),
                 'format' => config('eleven59.backpack-settings-extended.image.format'),
             ]);
-        } elseif (is_file ($value)) {
+        } elseif (strlen($value) < 512 && is_file ($value)) {
             $disk = config('eleven59.backpack-settings-extended.storage.disk');
             $path = config('eleven59.backpack-settings-extended.storage.destination_path');
             $this->uploadFileToDisk($value, "value", $disk, $path);
