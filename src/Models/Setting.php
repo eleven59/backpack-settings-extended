19ea99b36a24b7332d0624de2160f929805aaf3b
Nik Linders <info@eleven59.nl>
2024-02-24 14:14:13 +0100

    Added get function to custom model to make translations work when using Setting::get() as well

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -9,4 +9,23 @@ class Setting extends \Backpack\Settings\app\Models\Setting
     use HasTranslations;
 
     protected array $translatable = ['value'];
+
+    /**
+     * Grab a setting value from the database.
+     *
+     * @param string $key The setting key, as defined in the key db column
+     *
+     * @return string The setting value.
+     */
+    public static function get($key)
+    {
+        $setting = new self();
+        $entry = $setting->where('key', $key)->first();
+
+        if (!$entry) {
+            return;
+        }
+
+        return $entry->value;
+    }
 }
